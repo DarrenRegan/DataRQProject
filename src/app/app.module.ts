49fba7b7c0a855a40b511e53e280e4d257acb95c
Darren Regan <darren111894@gmail.com>
2018-11-24 23:26:46 +0000

    Added Bootstrap Styling for 4 component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PlaylistComponent } from './playlist/playlist.component';
+import { VideoListComponent } from './video-list/video-list.component';
+import { VideoDetailsComponent } from './video-details/video-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    PlaylistComponent
+    PlaylistComponent,
+    VideoListComponent,
+    VideoDetailsComponent
   ],
   imports: [
     BrowserModule,
