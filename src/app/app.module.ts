0c35e802277d6e7ccff3558b99dfa816f275b313
Darren Regan <darren111894@gmail.com>
2018-11-24 23:19:12 +0000

    Updated Angular html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PlaylistComponent } from './playlist/playlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PlaylistComponent
   ],
   imports: [
     BrowserModule,
