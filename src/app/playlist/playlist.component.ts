f3029211b60421571e7c7db06035b24d4718d17f
Darren Regan <darren111894@gmail.com>
2018-11-24 23:46:15 +0000

    Added more Compotent Styling

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Video } from '../video';
 
 @Component({
   selector: 'app-playlist',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./playlist.component.css']
 })
 export class PlaylistComponent implements OnInit {
+
+  //Array for 4 Videos to display to videolist.component.ts & bootstrap styling
+  videos: Video[] = [
+    {"_id": "1", "title": "Title 1", "url": "url 1", "description": "desc 1"},
+    {"_id": "2", "title": "Title 2", "url": "url 2", "description": "desc 2"},
+    {"_id": "3", "title": "Title 3", "url": "url 3", "description": "desc 3"},
+    {"_id": "4", "title": "Title 4", "url": "url 4", "description": "desc 4"}
+  ];
 
   constructor() { }
 
