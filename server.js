20949c144918884a24cdd5e72429f3359cb483de
Darren Regan <darren111894@gmail.com>
2018-12-02 18:41:48 +0000

    Fixed Access Control
    Added an additiional line of code missing along with "/api/" to remove Access Controll Error removing the need for chrome extension.

--- server.js
+++ server.js
@@ -14,14 +14,19 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.use(bodyParser.urlencoded({ extended: false })); 
 app.use(bodyParser.json());
 
-app.use('/api', api);
-
-app.use(function(req, res, next) {
+//"/api/", Removes assess control error
+//https://www.npmjs.com/package/body-parser
+app.use("/api/", function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept");
     next();
-});
+    });
+
+app.use('/api', api);
+
+
 
 //Render index.html
 app.get('*', (req, res) => {
